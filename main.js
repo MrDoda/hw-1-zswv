0eb08e20d3915ac42f437f356b81275354d98ac2
Daniel Král <kraldaniel.cze@gmail.com>
2023-10-05 12:25:27 +0200

    accomodate for negative numbers

--- main.js
+++ main.js
@@ -7,10 +7,15 @@ console.log('Add some example values:')
 
 const exampleValues = [
   { number: 'a', base: '16', desiredBase: '10' },
-  { number: '0111001', base: '2', desiredBase: '10' },
-  { number: '01100111', base: '2', desiredBase: '16' },
+  { number: '-a', base: '16', desiredBase: '10' },
+  { number: '011001011', base: '2', desiredBase: '16' },
   { number: '256', base: '10', desiredBase: '16' },
+  { number: '3333', base: '10', desiredBase: '16' },
   { number: 'AAA', base: '16', desiredBase: '2' },
+  { number: '0111001', base: '2', desiredBase: '10' },
+  { number: '-32158', base: '10', desiredBase: '16' },
+  { number: '-010110', base: '2', desiredBase: '10' },
+  { number: '22', base: '10', desiredBase: '2' },
 ]
 
 console.log('exampleValues', exampleValues)
--- src/conversion-helpers.js
+++ src/conversion-helpers.js
@@ -1,10 +1,20 @@
 export const hexToDecimal = (hexStr) => {
+  const isNumberNegative = hexStr[0] === '-'
+
+  if (isNumberNegative) {
+    hexStr = hexStr.substring(1)
+  }
+
   const hexDigits = '0123456789ABCDEF'
   hexStr = hexStr.toUpperCase()
   let decimal = 0
 
   for (let i = 0; i < hexStr.length; i++) {
     decimal = decimal * 16 + hexDigits.indexOf(hexStr[i])
+  }
+
+  if (isNumberNegative) {
+    decimal = -1 * decimal
   }
 
   return decimal
@@ -15,23 +25,42 @@ export const binaryToDecimal = (binStr) => {
 }
 
 export const decimalToHex = (decimal) => {
+  const isNumberNegative = decimal < 0
+
+  if (isNumberNegative) {
+    decimal = decimal * -1
+  }
+
   const hexDigits = '0123456789ABCDEF'
   let hex = ''
-
   while (decimal > 0) {
     hex = hexDigits[decimal % 16] + hex
     decimal = Math.floor(decimal / 16)
+  }
+
+  if (isNumberNegative) {
+    hex = `-${hex}`
   }
 
   return hex || '0'
 }
 
 export const decimalToBinary = (decimal) => {
+  const isNumberNegative = decimal < 0
+
+  if (isNumberNegative) {
+    decimal = decimal * -1
+  }
+
   let binary = ''
 
   while (decimal > 0) {
     binary = (decimal % 2) + binary
     decimal = Math.floor(decimal / 2)
+  }
+
+  if (isNumberNegative) {
+    binary = `-${binary}`
   }
 
   return binary || '0'
